b9287c3db494b8806a4b783eef46be1fc8ee0132
ranita224 <raniyaayalew@gmail.com>
2026-07-10 23:14:19 +0300

    total bill amount split in a group

--- day02.py
+++ day02.py
@@ -1,5 +1,5 @@
 # 1)write a python small python program that splits a Restaurant  bill with in a group of people 
-total = int(input("Enter the total bill amount: "))
+total = int(input("Enter the bill amount: "))
 number_of_people = int(input("Enter the number of people: "))
 def split_the_bill(total, number_of_people, tip_rate=0.1):
     total_with_tip = total + (total * tip_rate) 
