28178628d2a1efbcf20b70fa0d001c84ccd12b63
ranita224 <raniyaayalew@gmail.com>
2026-07-10 23:11:40 +0300

    Implement bill splitting program with tip and loop
    Added a program to split a restaurant bill with a tip rate using a loop.

--- day02.py
+++ day02.py
@@ -1,7 +1,18 @@
+# 1)write a python small python program that splits a Restaurant  bill with in a group of people 
 total = int(input("Enter the total bill amount: "))
 number_of_people = int(input("Enter the number of people: "))
 def split_the_bill(total, number_of_people, tip_rate=0.1):
     total_with_tip = total + (total * tip_rate) 
     return total_with_tip / number_of_people
 print(split_the_bill(total, number_of_people))
+#2) write a small python program that splits a restaurant bill with a tip rate=0.1 across friends 
+#using variables, operators, a function and  a loop.
+total = int(input("Enter the bill amount: "))
+number_of_people = int(input("Enter the number of people: "))
+def split_the_bill(total, number_of_people, tip_rate=0.1):
+    total_with_tip = total + (total * tip_rate) 
+    return total_with_tip / number_of_people
+for i in range(number_of_people):
+    print(f"the total bill is {split_the_bill(total, number_of_people)}")
+
 
